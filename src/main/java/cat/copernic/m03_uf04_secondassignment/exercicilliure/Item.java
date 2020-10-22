d48a1166a33bcb7dc0e1afac9bb170220b321712
Cristian_21 <mvcristian13@gmail.com>
2020-10-22 20:24:45 +0200

    Haciendo item

--- src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Item.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Item.java
@@ -18,5 +18,38 @@ package cat.copernic.m03_uf04_secondassignment.exercicilliure;
  * 
  */
 public class Item {
+    private double precio;
+    private String descripcion;
+    private String nombre;
+    private int id;
+
+    public Item(double precio, String descripcion, String nombre, int id) {
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "precio=" + precio + ", descripcion=" + descripcion + ", nombre=" + nombre + ", id=" + id + '}';
+    }
+    
     
 }
