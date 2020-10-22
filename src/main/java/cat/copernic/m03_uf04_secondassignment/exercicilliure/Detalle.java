dbe76b9d3f8dcac692391b0f12dcb42fae08c417
Cristian_21 <mvcristian13@gmail.com>
2020-10-22 20:32:03 +0200

    Haciendo detalle

--- src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Detalle.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Detalle.java
@@ -13,5 +13,15 @@ package cat.copernic.m03_uf04_secondassignment.exercicilliure;
  * 
  */
 public class Detalle {
+    private int id;
+    private String cantidad;
+    private Item item;
+
+    public Detalle(int id, String cantidad, Item item) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.item = item;
+    }
+    
     
 }
